8a33691592e06a14079ee7b455401da0f936288b
yuban10703 <yuban10703@foxmail.com>
2021-08-13 01:50:08 +0800

    change r18 func

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import re
-from typing import Set
+from typing import Set, Union
 
 from fastapi import FastAPI, Query, HTTPException
 
@@ -17,16 +17,16 @@ app = FastAPI(
 
 
 @app.get('/setu', response_model=Setu_out)
-async def setu_get(r18: bool = False,
+async def setu_get(r18: Union[bool, None] = None,
                    num: int = Query(1, ge=1, le=30),
                    tags: Set[str] = Query(set())):
-    condition = {'r18': r18}
+    condition: dict = {'r18': r18} if r18 else {}
     condition_and = []
     for tag in tags:
         if not tag.isspace():  # tag不为空时
             condition_and.append({'tags': re.compile(tag.strip(), re.I)})
     if condition_and:
-        condition['$and'] = condition_and
+        condition["$and"] = condition_and
     setus = await find_setu(condition, num)
     if not setus:
         raise HTTPException(status_code=404, detail="色图库中没找到色图~")
@@ -35,7 +35,7 @@ async def setu_get(r18: bool = False,
 
 @app.post("/setu", response_model=Setu_out)
 async def setu_post(item: Item):
-    condition = {'r18': item.r18}
+    condition: dict = {'r18': item.r18} if item.r18 else {}
     condition_and = []
     for tag in item.tags:
         if not tag.isspace():  # tag不为空时
--- app/model/setu_item.py
+++ app/model/setu_item.py
@@ -1,9 +1,9 @@
-from typing import Optional, Set
+from typing import Optional, Set, Union
 
 from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
-    r18: Optional[bool] = False
+    r18: Union[bool, None] = None
     num: Optional[int] = Field(1, ge=1, le=30)
     tags: Set[str] = set()
