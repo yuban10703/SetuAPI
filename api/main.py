27bc9063c2b3eb6341c581a7033c4fe1f9781348
yuban10703 <yuban10703@foxmail.com>
2021-08-12 21:56:10 +0800

    new start 9

--- api/main.py
+++ api/main.py
@@ -1,12 +1,20 @@
-import asyncio
-import os
 import re
 from typing import Optional, Set
 
-import motor.motor_asyncio
 from fastapi import FastAPI, Query, HTTPException
 from fastapi.responses import ORJSONResponse
 from pydantic import BaseModel, Field
+
+from database import DataBase
+
+db = DataBase()
+
+
+class Item(BaseModel):
+    r18: Optional[bool] = False
+    num: Optional[int] = Field(1, ge=1, le=30)
+    tags: Set[str] = set()
+
 
 app = FastAPI(
     title="setu",
@@ -16,25 +24,6 @@ app = FastAPI(
     # docs_url="/api/docs",
     # redoc_url="/api/redoc"
 )
-
-loop = asyncio.get_event_loop()
-
-client = motor.motor_asyncio.AsyncIOMotorClient(os.getenv('mongodb'), io_loop=loop)
-mydb = client[os.getenv('db')]
-collection = mydb[os.getenv('col')]
-
-
-async def find(condition: dict, num: int):
-    return [
-        data async for data in
-        collection.aggregate([{'$match': condition}, {'$sample': {'size': num}}, {'$project': {'_id': 0}}])
-    ]
-
-
-class Item(BaseModel):
-    r18: Optional[bool] = False
-    num: Optional[int] = Field(1, ge=1, le=30)
-    tags: Set[str] = set()
 
 
 @app.get('/setu', response_class=ORJSONResponse)
@@ -48,7 +37,7 @@ async def setu_get(r18: bool = False,
             condition_and.append({'tags': re.compile(tag.strip(), re.I)})
     if condition_and:
         condition['$and'] = condition_and
-    setus = await find(condition, num)
+    setus = await db.find(condition, num)
     if not setus:
         raise HTTPException(status_code=404, detail="色图库中没找到色图~")
     return {'code': 200, 'count': len(setus), 'tags': [i['tags'].pattern for i in condition_and], 'data': setus}
@@ -63,7 +52,7 @@ async def setu_post(item: Item):
             condition_and.append({'tags': re.compile(tag.strip(), re.I)})
     if condition_and:
         condition['$and'] = condition_and
-    setus = await find(condition, item.num)
+    setus = await db.find(condition, item.num)
     if not setus:
         raise HTTPException(status_code=404, detail="色图库中没找到色图~")
     return {'code': 200, 'count': len(setus), 'tags': [i['tags'].pattern for i in condition_and], 'data': setus}
@@ -74,7 +63,10 @@ async def hello():
     return {'message': 'Hello world!'}
 
 
-if __name__ == '__main__':
-    import uvicorn
+app.add_event_handler("startup", db.connect_db)
+app.add_event_handler("shutdown", db.close_db)
 
-    uvicorn.run('main:app', host="0.0.0.0", port=80, reload=True)
+# if __name__ == '__main__':
+#     import uvicorn
+#
+#     uvicorn.run('main:app', host="0.0.0.0", port=80, reload=True)
